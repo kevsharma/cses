f202173d59281dfe1c41cda23c5694f08efd9c02
kevsharma <>
2025-07-29 12:44:19 -0400

    Trailing zeroes

--- introductory/trailing_zeroes.cpp
+++ introductory/trailing_zeroes.cpp
@@ -1,45 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-
-pii last_digit_and_num_zeroes(int x) {
-    int zeroes = 0;
-    for (; x % 10 == 0; x /= 10) {
-        zeroes++;
-    }
-    return {x % 10, zeroes};
-}
-
-int solve(int n) {
-    int z = 0; // the number of zeroes
-    int mi = 1; // the multiplicative identity
-    pii p;
-    for (int i = 2; i <= n; i++) {
-        // Truncate multiplier and add zeroes
-        p = last_digit_and_num_zeroes(i);
-        z += p.second;
-
-        mi *= p.first;
-
-        // Truncate product and add zeroes
-        p = last_digit_and_num_zeroes(mi);
-        z += p.second;
-        mi = p.first;
-    }
-
-    return z;
-}
-
-int32_t main() {
-    fast_io;
-    int n; 
+// I didn't get this problem. I realized 2 * 5 = 10 but that wasn't enough.
+// Based on large factorials, I also noticed answer * 4 = large number!
+// https://japlslounge.com/posts/cses/trailing_zeros/1.htm
+int main() {
+    long n, current = 5, answer = 0;
     cin >> n;
-    cout << solve(n);
+
+    for (; current <= n; current *= 5) {
+        answer += n / current;
+    }
+
+    cout << answer << endl;
     return 0;
 }
